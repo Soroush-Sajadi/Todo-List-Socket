c76188bbe71ded78626f1d6c380cd72c9afe62e2
Soroush Sajadi <soorooshsadjadee@gmail.com>
2021-01-18 21:23:59 +0100

    input BP

---
+++ app/todo-list/src/components/Input.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+@Component({})
+export default class Input extends Vue {
+  @Prop() value: string;
+
+}
+</script>
+<style scoped>
+
+</style>
---
+++ app/todo-list/src/components/index.ts
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import Input from './Input.vue';
+
+Vue.component('a-input', Input)
